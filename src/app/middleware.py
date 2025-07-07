379a777865bd39cf6b897164be0544f985972054
twhjames <jamesteo2701@gmail.com>
2025-07-07 17:02:47 +0800

    Chore: Update CORS settings to allow Vite frontend

--- src/app/middleware.py
+++ src/app/middleware.py
@@ -6,7 +6,7 @@ def setup_middlewares(app: FastAPI):
     # allow_origins
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost", "http://localhost:3000"],
+        allow_origins=["http://localhost", "http://localhost:5173"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
